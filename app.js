01535a8bb4549e3dcec366785c7e310b18aa9dca
Евгений Дианов <dianov.js@gmail.com>
2022-12-10 16:51:24 +0300

    initial

---
+++ app.js
@@ -0,0 +1,12 @@
+MicroModal.init({
+  //   onShow: (modal) => console.info(`${modal.id} is shown`),
+  //   onClose: (modal) => console.info(`${modal.id} is hidden`),
+  //   openTrigger: "data-custom-open",
+  //   closeTrigger: "data-custom-close",
+  //   openClass: "is-open",
+  disableScroll: true,
+  //   disableFocus: false,
+  awaitOpenAnimation: false,
+  awaitCloseAnimation: false,
+  //   debugMode: true,
+});
